de7795640d95b737052099c14823769968a3b9b6
Erik Mikkelson <erik.mikkelson@blitzpick.co>
2017-03-14 22:52:20 +0000

    Native for android

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import { Platform, DrawerLayoutAndroid } from 'react-native';
+
+if (Platform.OS === 'android') {
+  module.exports = DrawerLayoutAndroid;
+} else {
+  module.exports = require('./DrawerLayout.ios').default;
+}
